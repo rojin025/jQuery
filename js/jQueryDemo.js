02abe1b1371670dabc12a7338ec69d940788e6b7
Rojin BIjukchhe <35796647+rojin025@users.noreply.github.com>
2018-03-11 21:02:50 -0700

    v.2.4
    final exercise done

--- js/jQueryDemo.js
+++ js/jQueryDemo.js
@@ -3,3 +3,9 @@ $("div").css("background","purple")
 
 //select the divs with class "highlight" and make them 200 px wide
 $(".highlight").css("width","200px")
+
+//select the divs with id "third" and give it a orange border
+$("#third").css("border","2px solid orange")
+
+//Bonus: Select the first div only and change its font color to red
+$("div:first-of-type").css("color","red")
