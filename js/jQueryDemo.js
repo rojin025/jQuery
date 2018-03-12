76107fa323980b68c604a521cdd7ffa5a0b42126
Rojin BIjukchhe <35796647+rojin025@users.noreply.github.com>
2018-03-11 20:56:41 -0700

    v.2.3

--- js/jQueryDemo.js
+++ js/jQueryDemo.js
@@ -1,1 +1,5 @@
+//Select all divs and give them a purple background
 $("div").css("background","purple")
+
+//select the divs with class "highlight" and make them 200 px wide
+$(".highlight").css("width","200px")
